3517057f7c2078c53bef2c8d4e98762c66bb0527
demirose41 <demirose41@gmail.com>
2024-11-21 16:04:12 -0800

    added createGrid and createSquare methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,33 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
+function createSquare(row, col) {
+    const square = document.createElement("div")
+    square.setAttribute("class", "square")
+    square.dataset.row = row;
+    square.dataset.col = col
+    return square
+}
+
+function createGrid(grid) {
+    const gridElement = document.createElement("div")
+    gridElement.setAttribute("class", "grid")
+
+    for(let x = 0; x < grid.length; x++){
+        let row = document.createElement("div")
+        row.setAttribute("class", "row")
+        row.setAttribute("id", `row-${x}`)
+        for( let y = 0; y < grid[0].length; y++){
+            let square = createSquare(x, y)
+            square.setAttribute("id", `square-${y}`)
+            row.appendChild(square)
+        }
+        gridElement.appendChild(row)
+    }
+    return gridElement    
+}
+
 // Your code here
+
+const grid = createGrid(board.grid)
+document.body.appendChild(grid)
